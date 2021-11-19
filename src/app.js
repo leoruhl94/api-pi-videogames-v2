b4ff5430178b93390ea34db55f0988d64128581e
leoruhl94 <leoruhl94@gmail.com>
2021-11-18 23:45:29 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
-const boom = require("@hapi/boom");
 const express = require('express');
 const morgan = require('morgan');
-const axios = require("axios");
 // const cookieParser = require('cookie-parser');
 const routes = require('./routes/index');
 const cors = require('cors')
@@ -31,33 +29,6 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(morgan('dev'));
 // app.use(cookieParser());
-
-app.get('/', (req, res, next)=>{
-  res.send('Hola parece que funciona')
-})
-app.get('/genres', async (req, res, next)=>{
-  try {
-    
-    let genresApi = await axios.get(
-      `https://api.rawg.io/api/genres?key=${process.env.API_KEY}`
-    );
-    res.json(genresApi.data.results)
-  } catch (error) {
-    next(error)
-  }
-})
-app.get('/platforms', async (req, res, next)=>{
-  try {
-    let platformsApi = await axios.get(
-      `https://api.rawg.io/api/platforms?key=${process.env.API_KEY}`
-    );
-    // console.log(platformsApi)
-    res.json(platformsApi.data.results)
-    
-  } catch (error) {
-    next(error)
-  }
-})
 
 app.use('/api', routes);
 // // Error handler*/
