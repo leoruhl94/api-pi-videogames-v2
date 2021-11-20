74264364077f0b33e76397c2f36a8ea01d114617
leoruhl94 <leoruhl94@gmail.com>
2021-11-19 22:07:00 -0300

    origenes añadidos a whitelist cors

--- src/app.js
+++ src/app.js
@@ -1,41 +1,48 @@
 require("dotenv").config();
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 // const cookieParser = require('cookie-parser');
-const routes = require('./routes/index');
-const cors = require('cors')
-const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/errorHandler')
+const routes = require("./routes/index");
+const cors = require("cors");
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+  ormErrorHandler,
+} = require("./middlewares/errorHandler");
 
 const app = express();
 
-app.name = 'API';
+app.name = "API";
 
-
-const whitelist = ['http://localhost:3000', 'https://the-games.herokuapp.com']
+const whitelist = [
+  "http://localhost:3000",
+  "https://the-games.herokuapp.com",
+  "http://the-games.herokuapp.com",
+  "http://192.168.0.114:3000/",
+];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('no permitido'));
+      callback(new Error("no permitido"));
     }
-  }
-}
+  },
+};
 
 app.use(cors(options));
- 
 
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(express.json({ limit: '50mb' }));
-app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(morgan("dev"));
 // app.use(cookieParser());
 
-app.use('/api', routes);
+app.use("/api", routes);
 // // Error handler*/
-app.use(logErrors)
-app.use(ormErrorHandler)
-app.use(boomErrorHandler)
-app.use(errorHandler)
-
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 module.exports = app;
